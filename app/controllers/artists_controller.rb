18b627708aa8510528f41e301a7c61c04839ff70
Ben Yellin <byellindc@gmail.com>
2019-02-01 16:11:30 -0500

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,33 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist = Artist.create(artist_params)
+    redirect_to artist
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    artist = Artist.update(params[:id], artist_params)
+    redirect_to artist
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
+end
