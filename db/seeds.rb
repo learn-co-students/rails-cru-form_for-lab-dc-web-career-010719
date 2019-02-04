eef416cbf718d1382e1883d6dc81269459233f21
Heloise Taillet <heloise96@hotmail.com>
2019-02-03 20:47:45 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+Artist.create([{ name: 'Britney Spears', bio: "Cute until she shaved her head" }, { name: 'Beyonce', bio: "she run the world"}])
+brit= Artist.first
+bey = Artist.last
+Genre.create([{name: "pop"}, {name: "rap"}])
+pop = Genre.first
+rap = Genre.last
+Song.create([{name: "Barbie Girl", artist_id: brit.id, genre_id: pop.id},{name: "Formation", artist_id: bey.id, genre_id: rap.id}])
